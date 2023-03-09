91e1e3e5e18538e69ca50af67494accc0b9464f9
k9998fuck <614403469@qq.com>
2023-03-09 16:18:36 +0800

    提交

--- app/src/main/java/com/k9998/MainActivity.java
+++ app/src/main/java/com/k9998/MainActivity.java
@@ -1,13 +1,10 @@
 package com.k9998;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,29 +19,26 @@ public class MainActivity extends AppCompatActivity {
         Log.d("lhc", "sdk_int " + Build.VERSION.SDK_INT);
     }
 
-    public void showDialog(View view){
+    public void showDialog(View view) {
         new AlertDialog.Builder(this)
-                .setTitle("setTitle").setMessage("setMessage").setPositiveButton("确定", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
+                .setTitle("标题")
+                .setMessage("内容")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-            }
-        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-            }
-        }).setNeutralButton("过一会", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).setNeutralButton("过一会", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-            }
-        }).show();
-//        Dialog dialog = new Dialog(this);
-//        dialog.setTitle(R.string.app_name);
-//        TextView textView = new TextView(this);textView.setText("123");
-//        dialog.setContentView(textView);
-//        dialog.show();
+                    }
+                }).show();
     }
 
 }
