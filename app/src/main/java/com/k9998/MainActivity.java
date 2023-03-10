e801958ebaa11efeb1a81fc3e8e289543f7eb0d1
k9998fuck <614403469@qq.com>
2023-03-10 18:00:52 +0800

    提交

--- app/src/main/java/com/k9998/MainActivity.java
+++ app/src/main/java/com/k9998/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("123");
         Log.d("lhc", "sdk_int " + Build.VERSION.SDK_INT);
+        TabHost tabHost = findViewById(android.R.id.tabhost);
+        tabHost.setup();
+        TabHost.TabContentFactory tabContentFactory = new TabHost.TabContentFactory() {
+            @Override
+            public View createTabContent(String tag) {
+                TextView textView = new TextView(MainActivity.this);
+                textView.setText(tag);
+                return textView;
+            }
+        };
+        tabHost.addTab(tabHost.newTabSpec("123").setIndicator("123").setContent(tabContentFactory));
+        tabHost.addTab(tabHost.newTabSpec("456").setIndicator("456").setContent(tabContentFactory));
+        tabHost.addTab(tabHost.newTabSpec("789").setIndicator("789").setContent(tabContentFactory));
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                Log.d("lhc", "tabId " + tabId);
+            }
+        });
     }
 
     public void showDialog(View view) {
